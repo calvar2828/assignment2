f23d088670659072100735a6a58c82348f883e44
calvar2828 <carlos.daa29@gmail.com>
2024-04-07 18:04:42 -0600

    update

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const registrationController = require('../controllers/registrationController');
 
 // Ruta para la página de inicio (formulario de registro)
 router.get('/', (req, res) => {
-    res.render('index');
+    //res.render('index');
+    res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 // Ruta para procesar el formulario de registro
